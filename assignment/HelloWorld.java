3ea92915c9fdce1780962a9a238ffe2307c241d3
taeyuning <121299307+taeyun11@users.noreply.github.com>
2023-02-22 01:49:05 +0900

    "hello, world"

---
+++ assignment/HelloWorld.java
@@ -0,0 +1,11 @@
+package assignment;/* *****************************************************************************
+ *  Name:              kim taeyun
+ *  Coursera User ID:
+ *  Last modified:     February 16th, 2023.
+ **************************************************************************** */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+}
---
+++ assignment/RandomWord.java
@@ -0,0 +1,27 @@
+/* *****************************************************************************
+ *  Name:              kim taeyun
+ *  Coursera User ID:
+ *  Last modified:     February 16th, 2023.
+ **************************************************************************** */
+package assignment;
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class RandomWord {
+    public static void main(String[] args) {
+        int index = 0;
+        String champion = "";
+
+        while (!StdIn.isEmpty()) {
+            String word = StdIn.readString();
+            boolean accept = StdRandom.bernoulli(1 / (index + 1.0));
+            if (accept) {
+                champion = word;
+            }
+            index++;
+        }
+        StdOut.println(champion);
+    }
+}
